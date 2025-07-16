d18005a5d7488302684145de15849d5d207a3e1d
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-07-16 19:53:40 -0300

    add limitations to login path

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { controller } from "./auth.controller.js";
-import { authenticateClient } from "../auth/authMiddleware.js";
+import { authenticateClient, onlyAnonymous } from "../auth/authMiddleware.js";
 
 export const authRouter = Router();
 
 authRouter.post('/password/recovery',authenticateClient, controller.resetPassword);
-authRouter.post('/login', controller.loginUser)
+authRouter.post('/login',onlyAnonymous, controller.loginUser)
 
