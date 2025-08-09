995f4d6ea7fbdc8755d82b954b1d4a7be1ba9d2c
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-08-09 11:24:58 -0300

    changed suppliers routes

--- src/supplier/supplier.routes.ts
+++ src/supplier/supplier.routes.ts
@@ -6,7 +6,10 @@ import { authenticateAdmin } from "../auth/authMiddleware.js";
 
 export const supplierRouter = Router();
 
+supplierRouter.get('/:cuit', authenticateAdmin,controller.findSupplierByCuit);
 supplierRouter.post('/restock/:id', authenticateAdmin, controller.requestRestock);
+supplierRouter.get('/by-id/:id', controller.findOne);
+supplierRouter.get('/:cuit', authenticateAdmin,controller.findSupplierByCuit);
 supplierRouter.get('/:cuit/products', authenticateAdmin, controller.findProductsBySupplier);
 supplierRouter.get('/:cuit', authenticateAdmin,controller.findSupplierByCuit);
 supplierRouter.get('/by-id/:id', controller.findOne);
