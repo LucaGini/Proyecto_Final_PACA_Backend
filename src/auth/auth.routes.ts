4e0ca32aa027e563f6d1d8398363371db96ab964
luisinagutierrez <luisinagutierrez2618@gmail.com>
2025-08-09 11:40:55 -0300

    changed routes password recovery middleware

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { controller } from "./auth.controller.js";
-import { authenticateClient, onlyAnonymous } from "../auth/authMiddleware.js";
+import { onlyAnonymous } from "../auth/authMiddleware.js";
 
 export const authRouter = Router();
 
-authRouter.post('/password/recovery', controller.resetPassword);
+authRouter.post('/password/recovery',onlyAnonymous, controller.resetPassword);
 authRouter.post('/login',onlyAnonymous, controller.loginUser)
 
